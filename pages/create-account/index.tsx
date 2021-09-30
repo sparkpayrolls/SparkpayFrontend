437784266b294c1bb370b300be94c5e2019ef48d
pelumi454 <amoochristianah454@gmail.com>
2021-09-29 20:53:01 -0700

    changes in create account page

--- pages/create-account/index.tsx
+++ pages/create-account/index.tsx
@@ -71,10 +71,13 @@ const CreateAccount: NextPage = () => {
             Already have an account?
             <span className="create-account__span-text"> Log In</span>
           </p>
-            <p className="create-account__terms-and-conditions">
-              By creating an account, you have agreed to our
-            </p>
-            <p className="">Terms and conditions</p>
+          <p className="create-account__terms-and-conditions">
+            By creating an account, you have agreed to our
+          </p>
+          <p className="create-account__terms-and-conditions">
+            <span className="create-account__span-text">Terms</span> and{" "}
+            <span className="create-account__span-text">conditions</span>
+          </p>
         </div>
       </div>
     </div>
