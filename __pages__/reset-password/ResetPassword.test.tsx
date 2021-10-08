1826b9127d3839c43eeddc37700f585f42ffbacb
Emmanuel Menyaga <sparklinsparky@gmail.com>
2021-10-08 09:24:13 +0100

    move test files from pages

--- __pages__/reset-password/ResetPassword.test.tsx
+++ __pages__/reset-password/ResetPassword.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
-import Home from './index';
+import ResetPassword from '../../pages/reset-password';
 
 configure({ adapter: new Adapter() });
-const app = shallow(<Home />);
+const app = shallow(<ResetPassword />);
 
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
