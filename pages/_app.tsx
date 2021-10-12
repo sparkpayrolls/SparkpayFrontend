70fcdf01a0fc75ff6280c8fc318ab254a6f37edc
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-10-12 10:26:06 +0100

    testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />  
       </Head>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
