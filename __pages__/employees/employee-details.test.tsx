35edd6509d816ad4ec7d7047a84e811b63393201
Ojonugwa Justice Alikali <alikali.ojonugwajustice@gmail.com>
2021-10-16 05:12:33 +0100

    (fix): organize sidebar SVG

--- __pages__/employees/employee-details.test.tsx
+++ __pages__/employees/employee-details.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import EmployeeDetails from './../index';
-
+import EmployeeDetails from '../../pages/employees/employee-details';
 
 it('renders correctly', () => {
-  const Employeedetails = renderer.create( <EmployeeDetails /> );
+  const Employeedetails = renderer.create(<EmployeeDetails />);
   expect(Employeedetails).toMatchSnapshot();
 });
