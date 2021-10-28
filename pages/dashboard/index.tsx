5d78db3f49786adef14c2726eaebd3b364905371
Ojonugwa Justice Alikali <alikali.ojonugwajustice@gmail.com>
2021-10-28 12:17:49 +0100

    (feat): Implement dashboard chart

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Line } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
 import withAuth from 'src/helpers/HOC/withAuth';
@@ -9,10 +11,53 @@ import wallet_icon from '/public/svgs/frame-32466.svg';
 import payroll_icon from '/public/svgs/frame-32468.svg';
 import employee_icon from '/public/svgs/frame-32469.svg';
 import organization_icon from '/public/svgs/frame-32465.svg';
+import arrow_down from '/public/svgs/arrow-down.svg';
 import wallet from '/public/images/wallet.png';
 import credit_card from '/public/images/credit-card.png';
 import cryptocurrency from '/public/images/cryptocurrency.png';
 // import { toast } from 'react-toastify';
+
+const lineData = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      fill: true,
+      backgroundColor: 'rgb(185 207 249 / 30%)',
+      borderColor: 'rgb(37 99 235)',
+    },
+  ],
+};
+
+const lineOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+      gridLines: {
+        drawBorder: false,
+      },
+    },
+  },
+  plugins: {
+    legend: false,
+  },
+};
+
+const doughnutData = {
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 10, 11],
+      backgroundColor: [
+        'rgba(103, 221, 228, 1)',
+        'rgba(243, 163, 88, 1)',
+        'rgba(255, 208, 77, 1)',
+        'rgba(130, 115, 217, 1)',
+      ],
+    },
+  ],
+};
 
 const Dashboard: NextPage = () => {
   // const notify = () => toast.warning('Wow so easy !', { delay: 1000 });
@@ -64,7 +109,56 @@ const Dashboard: NextPage = () => {
           </section>
 
           <section className="dashboard__chart-section">
-            <div>Chart</div>
+            <section className="disbursement">
+              <div className="disbursement__header">
+                <div className="disbursement__info">
+                  <h3 className="disbursement__title">Disbursement trend</h3>
+                  <span className="disbursement__date">
+                    August 20, 2021, 12:41 PM
+                  </span>
+                </div>
+
+                <button className="disbursement__select">
+                  Monthly <Image src={arrow_down} alt="dropdown" />
+                </button>
+              </div>
+
+              <Line data={lineData} options={lineOptions} />
+            </section>
+
+            <section className="payroll">
+              <Doughnut data={doughnutData} options={{ cutout: '75%' }} />
+
+              <div className="payroll__legend">
+                <div className="payroll__legend-item">
+                  <span className="payroll__legend-indicator">
+                    <LegendColorSVG color="#67DDE4" /> Payroll 1
+                  </span>
+                  <span className="payroll__legend-value">₦ 3.1m</span>
+                </div>
+
+                <div className="payroll__legend-item">
+                  <span className="payroll__legend-indicator">
+                    <LegendColorSVG color="#FFD04D" /> Payroll 2
+                  </span>
+                  <span className="payroll__legend-value">₦ 3.1m</span>
+                </div>
+
+                <div className="payroll__legend-item">
+                  <span className="payroll__legend-indicator">
+                    <LegendColorSVG color="#8273D9" /> Payroll 3
+                  </span>
+                  <span className="payroll__legend-value">₦ 3.1m</span>
+                </div>
+
+                <div className="payroll__legend-item">
+                  <span className="payroll__legend-indicator">
+                    <LegendColorSVG color="#F3A358" /> Payroll 4
+                  </span>
+                  <span className="payroll__legend-value">₦ 3.1m</span>
+                </div>
+              </div>
+            </section>
           </section>
 
           <section className="dashboard__transactions-section">
@@ -203,5 +297,17 @@ const FailedSvg = () => (
   >
     <circle cx="6" cy="6" r="6" fill="#FCE9E9" />
     <circle cx="6" cy="6" r="3" fill="#CA1B1B" />
+  </svg>
+);
+
+const LegendColorSVG = ({ color }: { color: string }) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="16" height="16" rx="4" fill={color} />
   </svg>
 );
