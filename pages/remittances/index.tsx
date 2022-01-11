e3e8e50f1a2daae6b184eb9771d41b8cb79ec3d8
tomike19 <kolajoelizabeth@gmail.com>
2022-01-10 21:01:25 -0800

    remittances screen

---
+++ pages/remittances/index.tsx
@@ -0,0 +1,72 @@
+import { NextPage } from 'next';
+import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
+
+
+
+export const RemittancesDetails =({
+  title,
+  details,
+} :{
+  title:string;
+  details:string;
+}) => {
+  return(
+<div className="remittances-page__remittances_details-section">
+<div>
+  <p className="remittances-page__remittances_details-text">{title}</p>   
+    <p className="remittances-page__remittances_details">{details}</p>
+  </div>
+</div>
+  );
+}
+
+export const NHFDetails =({
+  title,
+  details,
+} :{
+  title:string;
+  details:string;
+}) => {
+  return(
+<div className="remittances-page__NHF_details-section">
+<div>
+  <p className="remittances-page__NHF_details-text">{title}</p>   
+    <p className="remittances-page__NHF_details">{details}</p>
+  </div>
+</div>
+  );
+}
+const Remittances: NextPage = () => {
+  return (
+    <DashboardLayout pageTitle="remittances">
+      <div className="remittances-page">
+        <h1 className="remittances-page__remittances-header-title">
+         Remittance
+        </h1>
+        <p className="remittances-page__remittances-paragraph">3 remittances</p>
+        <div
+          className="remittances-page__remittances-cards
+    "
+        >
+            <RemittancesDetails
+            title="Tax"
+            details="Lorem sunt eiusmod officia incid excepteur 
+            aliquip cillum aute mollit aliqua dolor."
+            />
+             <NHFDetails
+            title="Pension"
+            details="Lorem sunt eiusmod officia incid excepteur 
+            aliquip cillum aute mollit aliqua dolor."
+            />
+             <RemittancesDetails
+            title="NHF"
+            details="Lorem sunt eiusmod officia incid excepteur
+             aliquip cillum aute mollit aliqua dolor."
+            />
+      </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Remittances;
