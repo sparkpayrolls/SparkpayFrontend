86ebc45034fa0a0c9095430f21c65b4b99799cea
tomike19 <kolajoelizabeth@gmail.com>
2022-01-19 18:29:33 -0800

    done with the remittances-tax-page

--- pages/remittances/remittances-tax.tsx
+++ pages/remittances/remittances-tax.tsx
@@ -1,31 +1,71 @@
 import { NextPage } from 'next';
 import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
 import { Button } from '../../src/components/Button/Button.component';
-
-
+import { Radio } from 'antd';
+import Image from 'next/image';
+import RemittancesInbox from "../../public/svgs/inbox.svg"
+import backicon from '../../public/svgs/back-icon.svg';
+import Link from 'next/link';
 
 const RemittancesTax: NextPage = () => {
   return (
     <DashboardLayout pageTitle="remittances-tax">
-      <div className="payroll-section__details">
-      <div className="remittances-tax-section">
-        <div className="payroll-section__head">
-          <h1 className="payroll-section__title">Tax</h1>
-          <div className="payroll-section__employee-button">
+      <div className="remittances-tax-page">
+        <div className="remittances-tax-section">
+          <div className="group-details__header-content">
+            <div className="group-details__group-detail-title-section"> 
+              <Link href="/remittances">
+                <a>
+                  <Image
+                    src={backicon}
+                    alt="group-details-image"
+                    className="group-details__prev-icon"
+                  />
+                </a>
+              </Link>
+              <h5 className="group-details__group-detail-title">Tax</h5>
+            </div>
             <Button
-              label={
+                label={
                 <>
                   {'Proceed'}
                 </>
               }
               element="a"
-              href="/payroll/create"
+              
               className="payroll-section__submit-btn"
               primary
               type="submit"
-            />
+              />
           </div>
-        </div>
+          <div className="remittances-tax-page__tax-details">
+            <div className="remittances-tax-page__remittances-options-group">
+              <h2>Required Actions</h2>
+              <p>You can only select on at a time</p>
+              <hr />
+              <Radio.Group name="uploadType">
+                <Radio value="Calculate">Calculate</Radio>
+                <br />
+                <Radio value="Deduct">Deduct</Radio>
+                <br />
+                <Radio value="Remit">Remit</Radio>
+              </Radio.Group>
+            </div>
+            <div className="remittances-tax-page__line"></div>
+            <div className="remittances-tax-page__General-information">
+              <h2>General Info</h2>
+              <p>Enter general info to calculate taxes</p>
+              <hr />
+              <div className="remittances-tax-page__remittances-inbox-image">
+                <Image
+                  src={RemittancesInbox}
+                  alt="remittances-inbox"
+                  
+                />
+                <p className="text-center" >Select one of the required Actions</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </DashboardLayout>
