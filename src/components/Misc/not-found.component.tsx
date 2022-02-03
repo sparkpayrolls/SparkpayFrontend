826733c48d07d35d22374c08e1d1f86508a7a1a7
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-02-04 00:18:38 +0100

    fix 403 error handling

--- src/components/Misc/not-found.component.tsx
+++ src/components/Misc/not-found.component.tsx
@@ -1,3 +1,5 @@
+import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
+
 export const NotFound = (props: { message: string }) => {
   return (
     <div className="not-found">
@@ -13,5 +15,27 @@ export const NotFound = (props: { message: string }) => {
         <h2 className="not-found__content__message">{props.message}</h2>
       </div>
     </div>
+  );
+};
+
+export const Forbidden = (props: { message?: string }) => {
+  return (
+    <DashboardLayout pageTitle="Forbidden">
+      <div className="not-found forbidden">
+        <div className="not-found__content">
+          <div className="deco-404">
+            <h3 className="deco-404__text">Forbidden!</h3>
+            <h1 className="deco-404__deco">
+              <span>4</span>
+              <span>0</span>
+              <span>3</span>
+            </h1>
+          </div>
+          <h2 className="not-found__content__message">
+            {props.message || 'You do not have permission to view this page'}
+          </h2>
+        </div>
+      </div>
+    </DashboardLayout>
   );
 };
