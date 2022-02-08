53333c4171eed0d9c3d4f6ef37341053dd4705cb
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-02-08 20:22:16 +0100

    container component

---
+++ src/components/Shared/container.component.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
+
+export const Container = (
+  props: PropsWithChildren<
+    DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+      loading?: boolean;
+    }
+  >,
+) => {
+  const { className, loading, children, ...divProps } = props;
+  const divClassName = classNames(className, 'app-container', {
+    'app-container--is-loading': loading,
+  });
+
+  return (
+    <div {...divProps} className={divClassName}>
+      {children}
+    </div>
+  );
+};
