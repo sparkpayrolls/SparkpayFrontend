b1475b9e86fd88b4e77530e1ac8d22515d8f5a28
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-02-16 14:30:16 +0100

    Update validation.ts

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -37,8 +37,8 @@ export const singleEmployeeUploadValidationSchema = Yup.object().shape({
   email: format.email,
   firstname: format.firstname,
   lastname: format.lastname,
-  salary: Yup.string().required('salary is required'),
-  phonenumber: Yup.string().required('phone number is required'),
+  salary: Yup.string().required('Salary is Required'),
+  phoneNumber: Yup.string().required('Phone Number is Required'),
 });
 
 export const bulkEmployeeFileUploadValidationSchema = Yup.object().shape({
