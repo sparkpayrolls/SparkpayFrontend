55fc85371062fab72695a38f14e600d430d65d22
pelumi454 <amoochristianah454@gmail.com>
2022-02-23 13:14:56 -0800

    fix: create textarea component

--- src/components/TextArea/TextArea.component.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// import TextArea from '@atlaskit/textarea';
-
-// export const TextArea = ({description}) => (
-//   <div>
-//     <h5>{ description}</h5>
-//   </div>
-// )
