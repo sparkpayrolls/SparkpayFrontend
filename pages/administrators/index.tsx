cde64611091c32a58d9d03c32227d270b5bd79d7
tomike19 <kolajoelizabeth@gmail.com>
2022-03-09 15:09:22 -0800

    Admin Role Table

---
+++ pages/administrators/index.tsx
@@ -0,0 +1,63 @@
+import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
+import type { NextPage } from 'next';
+import { PlusSvg } from '@/components/svg';
+import { Button } from '../../src/components/Button/Button.component';
+import { Tab } from '@/components/Tab/tab.component';
+import { useRouter } from 'next/router';
+import { stringifyUrl } from 'query-string';
+
+
+const validTabs = ['Admin Users', 'Roles'];
+
+
+const Admin: NextPage = () => {
+    const router = useRouter();
+  const tab = router.query.tab as string;
+  const activeTab = validTabs.includes(tab) ? tab : validTabs[0];
+
+  const onTabChange = (tab: string) => {
+    const { pathname, query } = router;
+    const url = stringifyUrl({
+      url: pathname,
+      query: { ...query, tab },
+    });
+
+    router.push(url);
+  };
+  return (
+    <DashboardLayout pageTitle="Admin">
+      <div className="employee-section">
+        <div className="employee-section__details">
+          <div className="employee-section__head">
+            <h1 className="employee-section__title">Admin Management</h1>
+            <div className="employee-section__employee-button">
+              <Button
+                label={
+                  <>
+                    <PlusSvg />
+                    &nbsp;{'Create Role'}
+                  </>
+                }
+                className="employee-section__submit-btn"
+                primary
+                type="submit"
+              />
+            </div>
+          </div>
+        </div>
+        <Tab
+            onChange={onTabChange}
+            active={activeTab}
+            default="admin"
+          >
+            <Tab.TabPane tab="Admin Users" key="Admin Users">
+            </Tab.TabPane>
+            <Tab.TabPane tab="Roles" key="Roles">
+            </Tab.TabPane>
+          </Tab>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Admin;
---
+++ src/components/admin/role.tsx
@@ -0,0 +1,19 @@
+import DashboardLayout from 'src/layouts/dashboard-layout/DashBoardLayout';
+import type { NextPage } from 'next';
+
+
+
+
+
+const AdminRole: NextPage = () => {
+  return (
+    <DashboardLayout pageTitle="Admin">
+      <div className="employee-section">
+       
+       
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default AdminRole;
