46576e5815b582a8e7d19c3b0ea6c63dacc18fcc
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-07-26 19:06:44 +0100

    git commit fix static generation

--- src/layouts/default-layout/DefaultLayout.tsx
+++ src/layouts/default-layout/DefaultLayout.tsx
@@ -17,6 +17,9 @@ import {
 import { useTawkto } from 'src/helpers/hooks/use-tawkto';
 
 function getWindowDimensions() {
+  if (typeof window === 'undefined') {
+    return { width: 0, height: 0 };
+  }
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
