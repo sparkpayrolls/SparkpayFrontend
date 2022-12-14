2e3bc92c944d8fe8ed9d76c32ca2274cb60303f2
Emmanuel Menyaga <sparklinsparky@gmail.com>
2022-12-14 16:35:25 +0100

    hot fix

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,64 +1,64 @@
 import { Text } from '@/components/Typography/Text';
-import { ChangeEvent, useEffect, useState } from 'react';
-import { Util } from 'src/helpers/util';
+// import { ChangeEvent, useEffect, useState } from 'react';
+// import { Util } from 'src/helpers/util';
 import DefaultLayout, { Title } from 'src/layouts/default-layout/DefaultLayout';
 
 export default function PricingPage() {
-  const [variables, setVariables] = useState({ amount: 1000000, size: 20 });
-  const [results, setResults] = useState({ payroll: 0, transfer: 0, total: 0 });
-  const [focused, setFocused] = useState({ amount: false, size: false });
-  const [priceComponent, setPriceComponent] = useState({
-    payroll: '',
-    transfer: '',
-  });
+  // const [variables, setVariables] = useState({ amount: 1000000, size: 20 });
+  // const [results, setResults] = useState({ payroll: 0, transfer: 0, total: 0 });
+  // const [focused, setFocused] = useState({ amount: false, size: false });
+  // const [priceComponent, setPriceComponent] = useState({
+  //   payroll: '',
+  //   transfer: '',
+  // });
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let { name, value } = event.target;
-    if (+value) {
-      value = +value as any;
-    }
+  // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   let { name, value } = event.target;
+  //   if (+value) {
+  //     value = +value as any;
+  //   }
 
-    setVariables({ ...variables, [name]: value });
-  };
+  //   setVariables({ ...variables, [name]: value });
+  // };
 
-  const handleBlur = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name } = event.target;
+  // const handleBlur = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const { name } = event.target;
 
-    setFocused({ ...focused, [name]: !focused[name as 'amount'] });
-  };
+  //   setFocused({ ...focused, [name]: !focused[name as 'amount'] });
+  // };
 
-  useEffect(() => {
-    const amount = Math.max(+variables.amount || 0, 0);
-    const size = Math.max(+variables.size || 0, 0);
-    let addition = 1000;
-    let capp = 3000;
-    let transferFee = 75;
-    if (size < 50) {
-      addition = 200;
-      capp = 2200;
-      transferFee = 55;
-    } else if (size < 500) {
-      addition = 500;
-      capp = 2500;
-      transferFee = 65;
-    }
+  // useEffect(() => {
+  //   const amount = Math.max(+variables.amount || 0, 0);
+  //   const size = Math.max(+variables.size || 0, 0);
+  //   let addition = 1000;
+  //   let capp = 3000;
+  //   let transferFee = 75;
+  //   if (size < 50) {
+  //     addition = 200;
+  //     capp = 2200;
+  //     transferFee = 55;
+  //   } else if (size < 500) {
+  //     addition = 500;
+  //     capp = 2500;
+  //     transferFee = 65;
+  //   }
 
-    const fee = Math.min((amount * 2.5) / 100 + addition, capp);
-    const totalTransferFee = transferFee * size;
-    setResults({
-      payroll: fee,
-      transfer: totalTransferFee,
-      total: amount + totalTransferFee + fee,
-    });
+  //   const fee = Math.min((amount * 2.5) / 100 + addition, capp);
+  //   const totalTransferFee = transferFee * size;
+  //   setResults({
+  //     payroll: fee,
+  //     transfer: totalTransferFee,
+  //     total: amount + totalTransferFee + fee,
+  //   });
 
-    setPriceComponent({
-      payroll:
-        fee === capp
-          ? `NGN ${Util.formatMoneyNumber(capp, 2)} capp`
-          : `2.5% + NGN ${Util.formatMoneyNumber(addition, 2)}`,
-      transfer: `NGN ${transferFee}/transfer`,
-    });
-  }, [variables]);
+  //   setPriceComponent({
+  //     payroll:
+  //       fee === capp
+  //         ? `NGN ${Util.formatMoneyNumber(capp, 2)} capp`
+  //         : `2.5% + NGN ${Util.formatMoneyNumber(addition, 2)}`,
+  //     transfer: `NGN ${transferFee}/transfer`,
+  //   });
+  // }, [variables]);
 
   return (
     <DefaultLayout>
