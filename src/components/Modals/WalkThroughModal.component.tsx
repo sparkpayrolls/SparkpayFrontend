42ece1fc00baf1c17d1ac3d69339702a8a9d060c
Emmanuel Menyaga <sparklinsparky@gmail.com>
2024-05-11 07:47:32 +0100

    chore

--- src/components/Modals/WalkThroughModal.component.tsx
+++ src/components/Modals/WalkThroughModal.component.tsx
@@ -14,12 +14,19 @@ export const WalkThroughModal = NiceModal.create(() => {
       className="WalkThrougModal"
     >
       <iframe
-        src="https://www.youtube.com/embed/QTUDwY03fYY?si=uuSK-IHmjK70YQho"
+        src="https://youtube.com/watch?v=83m-77tsCRQ&si=_7movIGrciGJXbqQ"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
+      {/* <iframe
+        src="https://www.youtube.com/embed/QTUDwY03fYY?si=uuSK-IHmjK70YQho"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe> */}
     </Modal>
   );
 });
