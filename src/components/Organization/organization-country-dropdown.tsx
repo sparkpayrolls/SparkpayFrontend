2de7db5427b6a50d510f3ab832a3e9a47379bd4d
tomike26 <kolajotomike26@gmail.com>
2024-11-21 18:04:41 +0100

    The country flag image changed

--- src/components/Organization/organization-country-dropdown.tsx
+++ src/components/Organization/organization-country-dropdown.tsx
@@ -1,19 +1,31 @@
 import { Formik } from 'formik';
-import Image from 'next/image';
 import { SelectInput } from '../Input/seletct-input';
 
-const CountryDropdown = (props:any) => {
+const CountryDropdown = (props: any) => {
   const { handleSubmit } = props;
 
   const initialValues = {
     country: '',
   };
   const countries = [
-    { label: 'Nigeria', value: 'Nigeria', imageUrl: '/svgs/NigeriaLogo.svg' },
-    { label: 'Kenya', value: 'Kenya', imageUrl: '/svgs/kenya.svg' },
-    { label: 'Ghana', value: 'Ghana', imageUrl: '/svgs/Ghana.svg' },
-    { label: 'Cameroon', value: 'CM', imageUrl: '/svgs/NigeriaLogo.svg' },
-    { label: 'Algeria', value: 'DZ', imageUrl: '/svgs/Kenya.svg' },
+    {
+      name: 'Nigeria',
+      iso2: 'NG',
+      flag: 'https://cdn.britannica.com/68/5068-050-53E22285/Flag-Nigeria.jpg',
+      id: '1',
+    },
+    {
+      name: 'Ghana',
+      iso2: 'GH',
+      flag: 'https://cdn.britannica.com/54/5054-050-8EC06097/Flag-Ghana.jpg',
+      id: '2',
+    },
+    {
+      name: 'Kenya',
+      iso2: 'KE',
+      flag: 'https://cdn.britannica.com/15/15-050-B075588A/Flag-Kenya.jpg',
+      id: '3',
+    },
   ];
 
   return (
@@ -31,21 +43,23 @@ const CountryDropdown = (props:any) => {
                 name="country"
                 placeholder="Select Country"
                 onBlur={handleBlur}
-                onChange={(e) => {
-                handleChange(e);
+                onChange={(event) => {
+                  handleChange(event);
+                  console.log('Selected country:', event.target.value);
                 }}
                 value={values.country}
                 options={countries.map((country) => ({
-                  value: country.value,
+                  value: country.iso2,
                   label: (
                     <div className="dropdown-option">
-                      <Image
-                        src={country.imageUrl}
-                        alt={country.label}
+                      <img
+                        src={country.flag}
+                        alt={`${country.name} flag`}
+                        className="country-flag"
                         width={20}
                         height={20}
                       />
-                      <span>{country.label}</span>
+                      <span>{country.name}</span>
                     </div>
                   ),
                 }))}
